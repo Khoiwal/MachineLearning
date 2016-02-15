fa2796a1043442bb6c0f29eed753e61ec752f497
Zac <bransonzac@gmail.com>
2016-02-15 15:28:50 -0500

    adding testable data and scripts

---
+++ HW3/torque_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+PBS -k o 
+PBS -l nodes=2:ppn=6,walltime=24:00:00
+PBS -M zbranson@indiana.edu
+PBS -m abe 
+PBS -N JobName 
+#PBS -j oe 
+
+#this should be a call to timbl
+#mpiexec -np 12 -machinefile $PBS_NODEFILE ~/bin/binaryname
